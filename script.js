4acd2717a9a8a404f063a51524051c562b81958d
c0deGirl <110123333+c0deGirl@users.noreply.github.com>
2022-11-20 13:29:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const largeSquares = document.querySelector('#large');
 const mediumSquares = document.querySelector('#medium');
 const smallSquares = document.querySelector('#small');
 const erease = document.querySelector('#erease');
-
+const colourDiv = document.querySelector('#colour');
+let colour = '#ceaa9a';
+const div = document.createElement('div');  
 
 let size = 20;
 for (let i = 0; i < size**2 ; i++) {    // This creates the exact number of divs inside the square
@@ -18,39 +20,52 @@ function createDiv(size) {
     div.style.height = `${430/size}px`; //height of the square divided by number of divs inside
     div.style.width = `${430/size}px`;  //width of the square divided by number of divs inside
     square.appendChild(div);
+    div.addEventListener('mouseenter', addColour);
     return div;
+    
 }
 
 
-largeSquares.addEventListener("click", function (e) {
+largeSquares.addEventListener("click", () => {
+    square.textContent = '';                // removing all divs inside the square
     size = 10;
     for (let i = 0; i < size**2 ; i++) {    // This creates the exact number of divs inside the square
         createDiv(size);
     } 
-    
 });
 
 
 mediumSquares.addEventListener("click", () => {
+    square.textContent = '';                // removing all divs inside the square
     size = 20;
-    for (let i = 0; i < size**2 ; i++) {
+    for (let i = 0; i < size**2 ; i++) {    // This creates the exact number of divs inside the square
         createDiv(size);
     }
 });
    
 smallSquares.addEventListener("click", () => {
+    square.textContent = '';                // removing all divs inside the square
     size = 50;
-    for (let i = 0; i < size**2 ; i++) {    
+    for (let i = 0; i < size**2 ; i++) {    // This creates the exact number of divs inside the square
         createDiv(size);
     }
 });
 
 
+erease.addEventListener("click", () => {    // clearing of the square
+    return square.textContent = '';
+})             
 
 
+function addColour () {
+    this.style.backgroundColor = colour;
+}
+
+function changeColour(choice) {
+    colour = choice;
+}
 
    
-
 
 
 
